ecdd466da25d7105e36ac185608ed83cef4315a9
lucas <892789989@qq.com>
2024-06-15 17:23:39 +0800

    add

--- vite.config.js
+++ vite.config.js
@@ -14,17 +14,5 @@ export default defineConfig((mode) => {
         "@": path.join(__dirname, "./src/"),
       },
     },
-    build: {
-      target: "es2015",
-      cssTarget: "chrome80",
-      minify: "terser", // 必须开启：使用terserOptions才有效果
-      terserOptions: {
-        compress: {
-          keep_infinity: true, // 防止 Infinity 被压缩成 1/0，这可能会导致 Chrome 上的性能问题
-          drop_console: true, // 生产环境去除 console
-          drop_debugger: true, // 生产环境去除 debugger
-        },
-      },
-    },
   };
 });

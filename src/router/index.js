99db40dfc6f3206e49022d5574423cc7ba264e4b
lucas <892789989@qq.com>
2024-06-15 17:05:41 +0800

    env

--- src/router/index.js
+++ src/router/index.js
@@ -5,7 +5,7 @@ const routes = [{ path: "/", component: Home }];
 
 const router = createRouter({
   // 4. 内部提供了 history 模式的实现。为了简单起见，我们在这里使用 hash 模式。
-  history: createWebHashHistory(),
+  history: createWebHashHistory(import.meta.env.VITE_PUBLIC_PATH),
   routes, // `routes: routes` 的缩写
 });
 
--- vite.config.js
+++ vite.config.js
@@ -1,14 +1,30 @@
-import { defineConfig } from "vite";
+import { defineConfig, loadEnv } from "vite";
 import UnoCSS from "unocss/vite";
 import vue from "@vitejs/plugin-vue";
 import path from "path";
 
-export default defineConfig({
-  base: "/me/",
-  plugins: [vue(), UnoCSS()],
-  resolve: {
-    alias: {
-      "@": path.join(__dirname, "./src/"),
+export default defineConfig((mode) => {
+  const env = loadEnv(mode, process.cwd());
+  const { VITE_PUBLIC_PATH } = env;
+  return {
+    base: VITE_PUBLIC_PATH,
+    plugins: [vue(), UnoCSS()],
+    resolve: {
+      alias: {
+        "@": path.join(__dirname, "./src/"),
+      },
     },
-  },
+    build: {
+      target: "es2015",
+      cssTarget: "chrome80",
+      minify: "terser", // 必须开启：使用terserOptions才有效果
+      terserOptions: {
+        compress: {
+          keep_infinity: true, // 防止 Infinity 被压缩成 1/0，这可能会导致 Chrome 上的性能问题
+          drop_console: true, // 生产环境去除 console
+          drop_debugger: true, // 生产环境去除 debugger
+        },
+      },
+    },
+  };
 });

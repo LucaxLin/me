f99982dd5ea84c814583131dd0c8a9845c169f0d
lucas <892789989@qq.com>
2024-06-22 15:07:36 +0800

    add

--- src/components/homeConfig.js
+++
@@ -1,9 +0,0 @@
-export const skills = {
-  HTML: "i-vscode-icons:file-type-html",
-  JavaScript: "i-vscode-icons:file-type-js-official",
-  CSS: "i-vscode-icons:file-type-css",
-  unocss: "i-vscode-icons:file-type-unocss",
-  'VUE2/3': "i-vscode-icons:file-type-vue",
-  Vite: "i-vscode-icons:file-type-vite",
-  Cypress: "i-vscode-icons:file-type-cypress",
-};

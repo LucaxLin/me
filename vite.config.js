b6a5153b9be1404a3e3bc0d0dd707e76f8f0636f
lucas <892789989@qq.com>
2024-07-20 15:49:34 +0800

    测试自动部署

--- vite.config.js
+++ vite.config.js
@@ -7,7 +7,7 @@ export default defineConfig((mode) => {
   const env = loadEnv(mode, process.cwd());
   const { VITE_PUBLIC_PATH } = env;
   return {
-    base: './',
+    base: VITE_PUBLIC_PATH,
     plugins: [vue(), UnoCSS()],
     resolve: {
       alias: {
